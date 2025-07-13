de5f32b88861eebe47b9b71afc93fe22f285d424
AbhishekSinghkirola <studyabhi82662@gmail.com>
2025-07-13 16:15:10 +0530

    apply refresh toen functionality

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -5,19 +5,67 @@ const axiosInstance = axios.create({
   withCredentials: true,
 });
 
-// axiosInstance.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("token");
-//     if (token) config.headers.Authorization = `Bearer ${token}`;
-//     return config;
-//   },
-//   (error) => Promise.reject(error)
-// );
-
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
     console.error("[Axios Error]", error?.response || error);
+    return Promise.reject(error);
+  }
+);
+
+let isRefreshing = false;
+let failedQueue = [];
+
+const processQueue = (error) => {
+  failedQueue.forEach(({ resolve, reject }) => {
+    if (error) {
+      reject(error);
+    } else {
+      resolve();
+    }
+  });
+  failedQueue = [];
+};
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (
+      error.response?.status === 401 &&
+      !originalRequest._retry &&
+      !originalRequest.url.includes("/users/refresh-token")
+    ) {
+      originalRequest._retry = true;
+
+      if (isRefreshing) {
+        return new Promise((resolve, reject) => {
+          failedQueue.push({
+            resolve: () => resolve(axiosInstance(originalRequest)),
+            reject: (err) => reject(err),
+          });
+        });
+      }
+
+      isRefreshing = true;
+
+      try {
+        await axiosInstance.post("/users/refresh-token");
+
+        isRefreshing = false;
+
+        processQueue(null);
+        return axiosInstance(originalRequest);
+      } catch (err) {
+        processQueue(err);
+        isRefreshing = false;
+
+        window.location.href = "/login";
+        return Promise.reject(err);
+      }
+    }
+
     return Promise.reject(error);
   }
 );
